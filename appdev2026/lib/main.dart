f903f7b1bf4b4098161f90d54981e2963a9c6b68
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-04-18 16:43:20 +0500

    Logo Widget added in widget folder.

--- appdev2026/lib/main.dart
+++ appdev2026/lib/main.dart
@@ -3,7 +3,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'auth/signin.dart';
 import 'auth/signup.dart';
 import 'firebase_options.dart';
---
+++ appdev2026/lib/test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SpendWiseLogo extends StatelessWidget {
+  final double fontSize;
+
+  const SpendWiseLogo({
+    super.key,
+    this.fontSize = 42.0, // Default large size for prominence
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        // Applying the base Google Font style
+        style: GoogleFonts.poppins(
+          fontSize: fontSize,
+          fontWeight: FontWeight.w800, // Extra bold for that "brand" look
+          letterSpacing: 1.0,
+        ),
+        children: [
+          TextSpan(
+            text: 'Spend',
+            style: TextStyle(
+              color: Colors.blue.shade400, // A vibrant, lighter blue
+              shadows: [
+                Shadow(
+                  color: Colors.blue.withOpacity(0.3),
+                  offset: const Offset(0, 2),
+                  blurRadius: 4,
+                ),
+              ],
+            ),
+          ),
+          TextSpan(
+            text: 'Wise',
+            style: TextStyle(
+              color: Colors.blue.shade900, // A deep, trustworthy navy blue
+            ),
+          ),
+          TextSpan(
+            text: '.',
+            style: TextStyle(
+              color:
+                  Colors.blueAccent, // A pop of color for an aesthetic period
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
