2b431c94cc2fc132e664fd2d29bc84f6d4da0359
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-05-22 08:09:36 +0500

    APK File Added and Favicon Updated.

--- lib/auth/google_auth_service.dart
+++ lib/auth/google_auth_service.dart
@@ -5,13 +5,14 @@ import 'package:google_sign_in/google_sign_in.dart';
 class GoogleAuthService {
   const GoogleAuthService();
 
-  Future<UserCredential> signInWithGoogle() async {
-    if (kIsWeb) {
-      final GoogleAuthProvider googleProvider = GoogleAuthProvider();
-      return FirebaseAuth.instance.signInWithPopup(googleProvider);
-    }
+  static final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: kIsWeb
+        ? '147458789912-o5494sgmj8gt8k6r31ciphto5rvrag33.apps.googleusercontent.com'
+        : null,
+  );
 
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
     if (googleUser == null) {
       throw FirebaseAuthException(
         code: 'google_sign_in_cancelled',
